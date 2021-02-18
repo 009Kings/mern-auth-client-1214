7e4e5214d0c8681fe08310d21f332775d1ead55a
009kings <009kings@gmail.com>
2021-02-18 14:20:25 -0800

    stubbed out that react router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import Content from './components/Content';
 import Header from './components/partials/Header';
 
-
 function App() {
   return (
     <div className="App">
+      {/* TODO Remove div, add styling more intentionally */}
       <Header />
       <Content />
     </div>
