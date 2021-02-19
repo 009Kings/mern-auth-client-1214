32e5c689a8700f6379654d90a55b530ee24f43cc
009kings <009kings@gmail.com>
2021-02-19 13:50:00 -0800

    authed users on signup!

--- src/components/partials/Signup.js
+++ src/components/partials/Signup.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import axios from 'axios';
 import setAuthToken from '../../utils/setAuthToken';
+import { Redirect } from 'react-router-dom';
 
 const Signup = (props) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   // TODO add form password verification
+  const [redirect, setRedirect] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -21,10 +23,12 @@ const Signup = (props) => {
       // set the token to auth header
       setAuthToken(response.data.token);
       // set user data
-      
+      props.handleAuth(response.data.user);
+      setRedirect(true);
     }).catch(err => console.log(`⛔️ Rutro:\n`, err));
   }
 
+  if (redirect) return <Redirect to='/profile' />
   return (
     <section>
       <h2>SIGNUP</h2>
